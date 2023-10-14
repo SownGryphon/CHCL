9f6406d52f98eef7228e3d5761e629a22d0f99d6
SownGryphon <zhenyastratford@gmail.com>
2023-10-14 14:15:02 +1100

    Set up premake

---
+++ premake5.lua
@@ -0,0 +1,53 @@
+workspace "CHCL"
+	architecture "x64"
+	configurations { "Debug", "Release" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CHCL"
+	location "CHCL"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}src/**.h",
+		"%{prj.name}src/**.cpp"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+project "CHCL Testing"
+	location "CHCL_Testing"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}src/**.h",
+		"%{prj.name}src/**.cpp"
+	}
+
+	includedirs
+	{
+		"CHCL/src"
+	}
+
+	links
+	{
+		"CHCL"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
