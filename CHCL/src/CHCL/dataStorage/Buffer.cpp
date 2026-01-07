dc974bb9c76b802e439aba23cc9635581d4118c8
SownGryphon <zhenyastratford@gmail.com>
2026-01-08 00:41:18 +1100

    Changes to Buffer usage

--- CHCL/src/CHCL/dataStorage/Buffer.cpp
+++ CHCL/src/CHCL/dataStorage/Buffer.cpp
@@ -1,34 +1,29 @@
 #include "Buffer.h"
 
 #include <cstring>
+#include <utility>
 
-CHCL::Buffer::Buffer(size_t presize)
+chcl::Buffer::Buffer(size_t presize)
 {
 	resize(presize, false);
 }
 
-CHCL::Buffer::Buffer(const void *data, size_t presize) :
+chcl::Buffer::Buffer(const void *data, size_t presize) :
 	Buffer(presize)
 {
 	append(data, presize);
 }
 
-CHCL::Buffer::Buffer(const Buffer &buffer) :
+chcl::Buffer::Buffer(const Buffer &buffer) :
 	Buffer(buffer.m_data, buffer.m_size)
 {}
 
-CHCL::Buffer::Buffer(Buffer &&buffer)
+chcl::Buffer::Buffer(Buffer &&buffer)
 {
-	m_data = buffer.m_data;
-	m_size = buffer.m_size;
-	m_capacity = buffer.m_capacity;
-
-	buffer.m_data = nullptr;
-	buffer.m_size = 0;
-	buffer.m_capacity = 0;
+	*this = std::move(buffer);
 }
 
-CHCL::Buffer::~Buffer()
+chcl::Buffer::~Buffer()
 {
 	if (*this)
 	{
@@ -36,29 +31,50 @@ CHCL::Buffer::~Buffer()
 	}
 }
 
-void CHCL::Buffer::reserve(size_t size)
+void chcl::Buffer::reserve(size_t size)
 {
 	if (size > m_size)
 		resize(size, true);
 }
 
-void CHCL::Buffer::write(size_t index, const void *data, size_t size)
+void chcl::Buffer::write(size_t index, const void *data, size_t size)
 {
 	std::memcpy(&m_data[index], data, size);
 }
 
-void CHCL::Buffer::append(const void *data, size_t size)
+void chcl::Buffer::append(const void *data, size_t size)
 {
 	write(m_size, data, size);
 	m_size += size;
 }
 
-CHCL::Buffer::operator bool() const
+chcl::Buffer::operator bool() const
 {
-	return m_capacity && m_data;
+	return m_size && m_data;
 }
 
-void CHCL::Buffer::resize(size_t newSize, bool preserveContents)
+chcl::Buffer& chcl::Buffer::operator=(const Buffer &other)
+{
+	resize(other.size(), false);
+	append(other.data(), other.size());
+	return *this;
+}
+
+chcl::Buffer& chcl::Buffer::operator=(Buffer &&other)
+{
+	if (*this)
+		delete[] m_data;
+
+	m_data = other.m_data;
+	m_size = other.m_size;
+
+	other.m_data = nullptr;
+	other.m_size = 0;
+
+	return *this;
+}
+
+void chcl::Buffer::resize(size_t newSize, bool preserveContents)
 {
 	uint8_t* newData = new uint8_t[newSize];
 
@@ -67,7 +83,9 @@ void CHCL::Buffer::resize(size_t newSize, bool preserveContents)
 		std::memcpy(newData, m_data, m_size);
 	}
 
-	delete[] m_data;
+	if (*this)
+		delete[] m_data;
+	
 	m_data = newData;
-	m_capacity = newSize;
+	m_size = newSize;
 }
--- CHCL/src/CHCL/dataStorage/Buffer.h
+++ CHCL/src/CHCL/dataStorage/Buffer.h
@@ -2,13 +2,13 @@
 
 #include <cstdint>
 
-namespace CHCL
+namespace chcl
 {
 	class Buffer
 	{
 	protected:
 		uint8_t *m_data = nullptr;
-		size_t m_size = 0, m_capacity = 0;
+		size_t m_size = 0;
 
 	public:
 		Buffer() = default;
@@ -23,15 +23,18 @@ namespace CHCL
 		void write(size_t index, const void *data, size_t size);
 		void append(const void *data, size_t size);
 
-		inline void* operator[](size_t index) { return m_data + index; }
-		inline const void* operator[](size_t index) const { return m_data + index; }
-
 		inline void* data() { return m_data; }
 		inline const void* data() const { return m_data; }
 
 		inline size_t size() const { return m_size; }
 
+		inline void* operator[](size_t index) { return m_data + index; }
+		inline const void* operator[](size_t index) const { return m_data + index; }
+
 		explicit operator bool() const;
+
+		Buffer& operator=(const Buffer &other);
+		Buffer& operator=(Buffer &&other);
 
 	private:
 
