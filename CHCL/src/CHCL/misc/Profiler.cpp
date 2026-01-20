95a01c27391dddbf1451c491dce4859240cf13dd
SownGryphon <zhenyastratford@gmail.com>
2026-01-20 13:47:13 +1100

    Profiler caching to improve performace
    Around a 2.5x decrease in time spent on profiling operations

--- CHCL/src/CHCL/misc/Profiler.cpp
+++ CHCL/src/CHCL/misc/Profiler.cpp
@@ -2,134 +2,125 @@
 
 #include "CHCL/Formatter.h"
 
-class _ProfileScope
+static chcl::Profiler::ProfilerEntry * const g_topLevelEntry = new chcl::Profiler::ProfilerEntry(chcl::Profiler::TopLevelName);
+// static std::vector<chcl::Profiler::ProfilerEntry*> g_entries = { g_topLevelEntry};
+static chcl::Profiler::ProfilerEntry *g_currentEntry = g_topLevelEntry;
+static bool g_profilerBad = false;
+
+void chcl::Profiler::Begin(ProfilerEntry *entry)
 {
-public:
-	using time_type = std::chrono::microseconds;
-	
-private:
-	bool m_running = false;
-	std::string m_scopeName;
+	if (g_profilerBad) return;
+	entry->begin();
 
-	std::chrono::steady_clock::time_point m_scopeBeginTime, m_pauseBegin;
-	time_type m_duration = time_type(0), m_pauseDuration = time_type(0);
-
-	_ProfileScope *m_parentScope = nullptr;
-	std::map<std::string, _ProfileScope*> m_childScopes;
-	_ProfileScope *m_lastChildScopeCache = nullptr;
-
-public:
-	_ProfileScope() = default;
-	_ProfileScope(const char *scopeName);
-	~_ProfileScope();
-
-	void begin();
-	void end();
-
-	void pause();
-	void unpause();
-
-	const std::string& getName() const { return m_scopeName; }
-	_ProfileScope* addChildScope(const char *name);
-	_ProfileScope* getParent() const { return m_parentScope; }
-
-	time_type getDuration() const { return m_duration; }
-	time_type getPauseDuration() const { return m_pauseDuration; }
-
-	void print(int depth = 0) const;
-};
-
-_ProfileScope g_topLevelScope = _ProfileScope(chcl::Profiler::TopLevelName);
-_ProfileScope *g_currentTopScope = &g_topLevelScope;
-
-chcl::Profiler::Scope::Scope(const char *name)
-{
-	Profiler::Begin(name);
+	g_currentEntry = entry;
 }
 
-chcl::Profiler::Scope::~Scope()
+void chcl::Profiler::End(ProfilerEntry *entry)
 {
-	Profiler::End();
-}
+	if (g_profilerBad) return;
 
-void chcl::Profiler::Begin(const char *scopeName)
-{
-	g_currentTopScope->pause();
-	_ProfileScope *newScope = g_currentTopScope->addChildScope(scopeName);
-	newScope->begin();
-	g_currentTopScope->unpause();
-
-	g_currentTopScope = newScope;
-}
-
-void chcl::Profiler::End(const char *name)
-{
-	g_currentTopScope->pause();
-	if (name && std::strcmp(g_currentTopScope->getName().c_str(), name))
+	if (g_currentEntry != entry)
 	{
-		g_currentTopScope->unpause();
-		std::cout << "BAD\n";
+		std::cout << Formatter::Format("Profiler BAD: got {}, expected {}\n", entry->getName(), g_currentEntry->getName());
+		while (g_currentEntry != g_topLevelEntry)
+		{
+			g_currentEntry->end();
+			g_currentEntry = g_currentEntry->getParentEntry();
+		}
+		g_profilerBad = true;
 		return;
 	}
 
-	g_currentTopScope->unpause();
-	g_currentTopScope->end();
-
-	g_currentTopScope = g_currentTopScope->getParent();
+	g_currentEntry->end();
+	g_currentEntry = g_currentEntry->getParentEntry();
 }
 
-void chcl::Profiler::PrintAllTimes()
+void chcl::Profiler::PrintTree()
 {
-	g_topLevelScope.print();
+	g_topLevelEntry->print();
 }
 
-void _ProfileScope::print(int depth) const
+void chcl::Profiler::ProfilerEntry::print(int depth) const
 {
-	std::cout << chcl::Formatter::Format("{0}- {1}: {2} ({3})\n", std::string(depth * 2, ' '), m_scopeName, m_duration, m_pauseDuration);
+	std::cout << chcl::Formatter::Format("{}- {}: {}\n", std::string(depth * 2, ' '), m_scopeName, m_duration);
 
 	time_type child_cumulative = time_type(0);
-	for (auto const &[name, scope] : m_childScopes)
+	for (auto const &[name, entry] : m_childEntries)
 	{
-		scope->print(depth + 1);
-		child_cumulative += scope->getDuration();
+		entry->print(depth + 1);
+		child_cumulative += entry->getDuration();
 	}
 
-	if (m_childScopes.size() > 0 && child_cumulative < m_duration)
-		std::cout << chcl::Formatter::Format("  {0}- *other: {1}\n", std::string(depth * 2, ' '), m_duration - child_cumulative);
+	if (m_childEntries.size() > 0 && child_cumulative < m_duration)
+		std::cout << chcl::Formatter::Format("  {}- *other: {}\n", std::string(depth * 2, ' '), m_duration - child_cumulative);
 }
 
-_ProfileScope::_ProfileScope(const char *scopeName) :
-	m_scopeName(scopeName)
+chcl::Profiler::ProfilerEntry::ProfilerEntry(const char *scopeName) :
+	m_scopeName(scopeName),
+	m_numericID(++s_idIncrementer)
 {}
 
-_ProfileScope::~_ProfileScope()
+#if 0
+#if 0
+chcl::Profiler::ProfilerEntry::ProfilerEntry(const ProfilerEntry &other) :
+	m_numericID(other.m_numericID),
+	m_scopeName(other.m_scopeName),
+	m_running(other.m_running),
+	m_scopeBeginTime(other.m_scopeBeginTime),
+	m_duration(other.m_duration),
+	m_parentScopeIndex(other.m_parentScopeIndex),
+	m_childEntryIndices(other.m_childEntryIndices)
 {
-	for (auto &[name, childPtr] : m_childScopes)
-	{
-		delete childPtr;
-	}
+	std::cout << Formatter::Format("Copy ctor size: {}, parent size: {}\n", m_childEntryIndices.size(), other.m_childEntryIndices.size());
 }
 
-void _ProfileScope::begin()
+chcl::Profiler::ProfilerEntry::ProfilerEntry(ProfilerEntry &&other) :
+	m_numericID(other.m_numericID),
+	m_scopeName(other.m_scopeName),
+	m_running(other.m_running),
+	m_scopeBeginTime(other.m_scopeBeginTime),
+	m_duration(other.m_duration),
+	m_parentScopeIndex(other.m_parentScopeIndex),
+	m_childEntryIndices(std::move(other.m_childEntryIndices))
+{
+	other.m_numericID = 0;
+	other.m_parentScopeIndex = 0;
+	std::cout << Formatter::Format("Move ctor size: {}, parent size: {}\n", m_childEntryIndices.size(), other.m_childEntryIndices.size());
+}
+#else
+chcl::Profiler::ProfilerEntry::ProfilerEntry(const ProfilerEntry &other)
+{
+	*this = other;
+}
+
+chcl::Profiler::ProfilerEntry::ProfilerEntry(ProfilerEntry &&other)
+{
+	*this = std::move(other);
+}
+#endif
+#endif
+
+chcl::Profiler::ProfilerEntry::~ProfilerEntry()
+{
+	// TODO: Actually implement well
+	// for (auto &[name, childIndex] : m_childEntryIndices)
+	// {
+	// 	g_entries.erase(g_entries.begin() + childIndex);
+	// }
+}
+
+void chcl::Profiler::ProfilerEntry::begin()
 {
 	m_running = true;
 	m_scopeBeginTime = std::chrono::steady_clock::now();
 }
 
-void _ProfileScope::end()
+void chcl::Profiler::ProfilerEntry::end()
 {
 	if (!m_running)
 		return;
-	
-	pause();
-	for (auto &child : m_childScopes)
-	{
-		child.second->end();
-	}
 
 	m_running = false;
-
-	unpause();
 
 	time_type scopeDuration = std::chrono::duration_cast<time_type>(std::chrono::steady_clock::now() - m_scopeBeginTime);
 
@@ -140,33 +131,108 @@ void _ProfileScope::end()
 	// m_pauseDuration = time_type(0);
 }
 
-void _ProfileScope::pause()
+#if 0
+void chcl::Profiler::ProfilerEntry::pause()
 {
 	m_pauseBegin = std::chrono::steady_clock::now();
 }
 
-void _ProfileScope::unpause()
+void chcl::Profiler::ProfilerEntry::unpause()
 {
 	time_type pauseDuration = std::chrono::duration_cast<time_type>(std::chrono::steady_clock::now() - m_pauseBegin);
 	m_pauseDuration += pauseDuration;
 	// std::cout << "Paused for " << pauseDuration << '\n';
 }
+#endif
 
-_ProfileScope* _ProfileScope::addChildScope(const char *name)
+chcl::Profiler::ProfilerEntry* chcl::Profiler::ProfilerEntry::getChildEntry(const char *name)
 {
-	if (m_lastChildScopeCache && !std::strcmp(m_lastChildScopeCache->getName().c_str(), name))
-		return m_lastChildScopeCache;
+	ProfilerEntry *res = m_childEntries[name];
 
-	_ProfileScope *child = m_childScopes[name];
+	if (res == nullptr)
+		res = addChildEntry(name);
 
-	if (child == nullptr)
-	{
-		child = new _ProfileScope(name);
-		child->m_parentScope = this;
-		m_childScopes[name] = child;
-	}
+	return res;
+}
 
-	m_lastChildScopeCache = child;
+chcl::Profiler::ProfilerEntry* chcl::Profiler::ProfilerEntry::addChildEntry(const char *name)
+{
+	ProfilerEntry *child = new ProfilerEntry(name);
+
+	// g_entries.push_back(child);
+	m_childEntries[name] = child;
+	child->m_parentEntry = this;
 
 	return child;
+}
+
+#if 0
+chcl::Profiler::ProfilerEntry& chcl::Profiler::ProfilerEntry::operator =(const ProfilerEntry &other)
+{
+	m_numericID = other.m_numericID;
+	m_scopeName = other.m_scopeName;
+	m_running = other.m_running;
+	m_scopeBeginTime = other.m_scopeBeginTime;
+	m_duration = other.m_duration;
+	m_parentScopeIndex = other.m_parentScopeIndex;
+	m_childEntryIndices = other.m_childEntryIndices;
+
+	return *this;
+}
+
+chcl::Profiler::ProfilerEntry& chcl::Profiler::ProfilerEntry::operator =(ProfilerEntry &&other)
+{
+	m_numericID = other.m_numericID;
+	m_scopeName = other.m_scopeName;
+	m_running = other.m_running;
+	m_scopeBeginTime = other.m_scopeBeginTime;
+	m_duration = other.m_duration;
+	m_parentScopeIndex = other.m_parentScopeIndex;
+	m_childEntryIndices = std::move(other.m_childEntryIndices);
+
+	return *this;
+}
+#endif
+
+chcl::Profiler::EntryCache::EntryCache(const char *name) :
+	m_name(name)
+{}
+
+#if 0
+unsigned int chcl::Profiler::EntryCache::getIndex()
+{
+	unsigned int res = m_entryMap[GetCurrentEntry()->getNumericID()];
+	std::cout << "Cache initial get success\n";
+
+	if (res == 0)
+	{
+		std::cout << "Cache needs new entry\n";
+		res = GetCurrentEntry()->getChildEntryIndex(m_name.c_str());
+	}
+
+	return res;
+}
+#endif
+
+chcl::Profiler::ProfilerEntry* chcl::Profiler::EntryCache::get()
+{
+	ProfilerEntry *res = m_entryMap[g_currentEntry->getNumericID()];
+
+	if (res != nullptr)
+		return res;
+
+	m_entryMap[g_currentEntry->getNumericID()] = g_currentEntry->getChildEntry(m_name.c_str());
+
+	return m_entryMap[g_currentEntry->getNumericID()];
+}
+
+chcl::Profiler::ScopeAutoProfiler::ScopeAutoProfiler(EntryCache *cache) :
+	m_entry(cache->get())
+{
+	Profiler::Begin(m_entry);
+}
+
+chcl::Profiler::ScopeAutoProfiler::~ScopeAutoProfiler()
+{
+	Profiler::End(m_entry);
 }
--- CHCL/src/CHCL/misc/Profiler.h
+++ CHCL/src/CHCL/misc/Profiler.h
@@ -2,32 +2,95 @@
 
 #include <chrono>
 #include <map>
+#include <unordered_map>
 #include <string>
 
 namespace chcl
 {
 	namespace Profiler
 	{
-		class Scope
+		class ProfilerEntry
 		{
 		public:
-			Scope(const char *name);
-			~Scope();
+			using time_type = std::chrono::microseconds;
+			
+		private:
+			inline static unsigned int s_idIncrementer = 0;
+			unsigned int m_numericID = 0;
+
+			std::string m_scopeName;
+			
+			bool m_running = false;
+			std::chrono::steady_clock::time_point m_scopeBeginTime; //, m_pauseBegin;
+			time_type m_duration = time_type(0); //, m_pauseDuration = time_type(0);
+
+			ProfilerEntry *m_parentEntry = nullptr;
+			std::map<std::string, ProfilerEntry*> m_childEntries = {};
+
+		public:
+			ProfilerEntry() = delete;
+			ProfilerEntry(const char *scopeName);
+			// ProfilerEntry(const ProfilerEntry&);
+			// ProfilerEntry(ProfilerEntry&&);
+			~ProfilerEntry();
+
+			void begin();
+			void end();
+
+			// void pause();
+			// void unpause();
+
+			inline unsigned int getNumericID() const { return m_numericID; }
+			inline const std::string& getName() const { return m_scopeName; }
+			ProfilerEntry* getChildEntry(const char *name);
+			ProfilerEntry* addChildEntry(const char *name);
+			inline ProfilerEntry* getParentEntry() const { return m_parentEntry; }
+
+			inline time_type getDuration() const { return m_duration; }
+			// inline time_type getPauseDuration() const { return m_pauseDuration; }
+
+			void print(int depth = 0) const;
+
+			// ProfilerEntry& operator =(const ProfilerEntry&);
+			// ProfilerEntry& operator =(ProfilerEntry&&);
+		};
+
+		class EntryCache
+		{
+		private:
+			std::unordered_map<unsigned int, ProfilerEntry*> m_entryMap;
+			std::string m_name;
+		public:
+			EntryCache(const char *name);
+
+			ProfilerEntry* get();
+		};
+
+		class ScopeAutoProfiler
+		{
+		private:
+			ProfilerEntry *m_entry;
+		public:
+			ScopeAutoProfiler(EntryCache *cache);
+			~ScopeAutoProfiler();
 		};
 
 		constexpr char TopLevelName[] = "all";
 
-		void Begin(const char *layerName);
-		void End(const char *name = nullptr);
+		void Begin(ProfilerEntry *entry);
+		void End(ProfilerEntry *entry);
 
-		void PrintAllTimes();
+		void PrintTree();
 	}
 }
 
 #ifdef CHCL_ENABLE_PROFILING
-	#define ProfileBegin(name) chcl::Profiler::Begin(#name);
-	#define ProfileEnd(name) chcl::Profiler::End(#name);
-	#define ProfileScope(name) chcl::Profiler::Scope _profileScope_##name{ #name };
+	#define ProfileBegin(name) static chcl::Profiler::EntryCache _chclProfilerCache_##name{ #name }; \
+		chcl::Profiler::ProfilerEntry *_chclProfilerCacheEntry_##name = _chclProfilerCache_##name.get();\
+		chcl::Profiler::Begin(_chclProfilerCacheEntry_##name);
+	#define ProfileEnd(name) chcl::Profiler::End(_chclProfilerCacheEntry_##name);
+	#define ProfileScope(name) static chcl::Profiler::EntryCache _chclProfilerCache_##name{ #name }; \
+		chcl::Profiler::ScopeAutoProfiler _profileScope_##name{ &_chclProfilerCache_##name };
 #else
 	#define ProfileBegin(name)
 	#define ProfileEnd(name)
